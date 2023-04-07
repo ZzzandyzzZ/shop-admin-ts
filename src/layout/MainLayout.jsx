070989b223aa09ffa82c7220cc29cadfe13ccef8
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-06 20:18:51 -0500

    Create and add main layout

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,9 @@
+export const MainLayout = ({ children }) => {
+  return (
+    <>
+      <nav>NAV</nav>
+      {children}
+      <footer>Footer</footer>
+    </>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MainLayout } from '@layout/MainLayout'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  )
 }
 
 export default App
