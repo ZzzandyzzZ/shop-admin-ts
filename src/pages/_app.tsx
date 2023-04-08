53c294953ddaf85d434bd70dbf6f59452e2dfd97
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-08 10:39:15 -0500

    Add auth provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import MainLayout from '@layout/MainLayout'
+import { AuthProvider } from 'context/AuthContext'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <AuthProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </AuthProvider>
   )
 }
 
