aeb0d716b145c2cd32e49f9ba5d9c4b2a18ec414
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-08 16:17:56 -0500

    Handle login error or success

--- src/hooks/useAuthProvider.tsx
+++ src/hooks/useAuthProvider.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { useState } from 'react'
 import endPoints from 'services/api'
+import Cookie from 'js-cookie'
 
 export const useAuthProvider = (): AuthContext => {
   const [user, setUser] = useState<User | null>(null)
+  const [loginStatus, setLoginStatus] = useState<LoginStatus>('pending')
   const signIn = async ({ email, password }: Credentials): Promise<void> => {
     const options = {
       headers: {
@@ -11,13 +13,25 @@ export const useAuthProvider = (): AuthContext => {
         'Content-Type': 'application/json',
       },
     }
-    const {
-      data: { access_token: token },
-    } = await axios.post(endPoints.auth.login, { email, password }, options)
-    console.log({ token })
+    try {
+      const { data } = await axios.post(
+        endPoints.auth.login,
+        { email, password },
+        options
+      )
+      Cookie.set('token', data.access_token, { expires: 5 })
+      setLoginStatus('correct')
+    } catch (error) {
+      setLoginStatus('error')
+    }
+  }
+  const resetLoginStatus = (): void => {
+    setLoginStatus('pending')
   }
   return {
     user,
+    loginStatus,
+    resetLoginStatus,
     signIn,
   }
 }
