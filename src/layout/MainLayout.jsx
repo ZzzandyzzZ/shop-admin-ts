4d8b4a84103cee8960c28e3336a7028180f7f002
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-08 10:39:33 -0500

    Fix Style  in main layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,21 +1,17 @@
 // import Header from "@components/Header";
 // import Nav from "@common/Nav";
 
-import Header from '@components/Header';
-import Navbar from '@components/Navbar';
+import Header from '@components/Header'
+import Navbar from '@components/Navbar'
 
-export default function MainLayout({children}) {
-    return(
-        <>
-        <div className="min-h-full">
-            <Header/>
-            <Navbar />
-            <main>
-                <div className="max-w-7xl mx-auto py-6 sm:px-6">
-                    {children}
-                </div>
-            </main>
-        </div>
-        </>
-    )
-};
+export default function MainLayout({ children }) {
+  return (
+    <div className="min-h-full">
+      <Header />
+      <Navbar />
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6">{children}</div>
+      </main>
+    </div>
+  )
+}
