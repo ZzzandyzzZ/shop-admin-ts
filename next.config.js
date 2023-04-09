6984143766b77d49c8ac898e409f5d3a0a284971
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-09 12:41:39 -0500

    Add doamins to get image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'api.lorem.space',
+      'cdna.artstation.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
