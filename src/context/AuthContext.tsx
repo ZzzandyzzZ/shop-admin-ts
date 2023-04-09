91f2e6dce9fb9dcaee7e95fde1ff8447d24478d9
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-08 20:09:52 -0500

    Add default value to AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,12 @@
 import { createContext } from 'react'
 import { useAuthProvider } from '@hooks/useAuthProvider'
 
-export const AuthContext = createContext<AuthContext | null>(null)
+export const AuthContext = createContext<IAuthContext>({
+  user: null,
+  loginStatus: 'pending',
+  resetLoginStatus: () => {},
+  signIn: async (credentials: Credentials) => {},
+})
 
 export const AuthProvider = ({ children }: AuthProviderProps): JSX.Element => {
   return (
