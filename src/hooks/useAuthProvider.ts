fbe97e4629d57b9bfb3ef25f14b6ff123083245b
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-08 20:09:32 -0500

    Add fetching data on page load

--- src/hooks/useAuthProvider.ts
+++ src/hooks/useAuthProvider.ts
@@ -1,11 +1,27 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import endPoints from 'services/api'
 import Cookie from 'js-cookie'
+import { useRouter } from 'next/router'
+import { tokenCookieName } from 'contants/user'
 
-export const useAuthProvider = (): AuthContext => {
+export const useAuthProvider = (): IAuthContext => {
   const [user, setUser] = useState<User | null>(null)
   const [loginStatus, setLoginStatus] = useState<LoginStatus>('pending')
+  const router = useRouter()
+
+  useEffect(() => {
+    const token = Cookie.get(tokenCookieName)
+    if (token === undefined || loginStatus === 'correct') return
+    const signInFromToken = async (token: string): Promise<void> => {
+      axios.defaults.headers.Authorization = `Bearer ${token}`
+      const { data } = await axios.get<User>(endPoints.auth.profile)
+      setUser(data)
+      setLoginStatus('correct')
+    }
+    void signInFromToken(token)
+  }, [loginStatus, user])
+
   const signIn = async ({ email, password }: Credentials): Promise<void> => {
     const options = {
       headers: {
@@ -21,21 +37,21 @@ export const useAuthProvider = (): AuthContext => {
         { email, password },
         options
       )
-      Cookie.set('token', token, { expires: 5 })
+      Cookie.set(tokenCookieName, token, { expires: 5 })
       axios.defaults.headers.Authorization = `Bearer ${token}`
+      const { data } = await axios.get<User>(endPoints.auth.profile)
+      setUser(data)
       setLoginStatus('correct')
-      const {
-        data: { user },
-      } = await axios.get<AuthProfileApi>(endPoints.auth.profile)
-      console.log({ user })
-      setUser(user)
+      await router.push('/dashboard')
     } catch (error) {
       setLoginStatus('error')
     }
   }
+
   const resetLoginStatus = (): void => {
     setLoginStatus('pending')
   }
+
   return {
     user,
     loginStatus,
