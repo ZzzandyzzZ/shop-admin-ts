4f5556132a08b66013187e811ce447cef557b666
ZzzandyzzZ <andy_gean@hotmail.com>
2023-04-09 16:04:35 -0500

    Add bar chart in dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import Chart from '@components/common/Chart'
 import useFetch from '@hooks/useFetch'
 import Image from 'next/image'
 import endPoints from 'services/api'
@@ -9,8 +10,24 @@ export default function Dashboard(): JSX.Element {
   const products = useFetch<IProductsApiData>(
     endPoints.products.getProducts(API_PRODUCT_LIMIT, API_PRODUCT_OFFSET)
   )
+  const categoryName = products?.map((product) => product.category.name)
+  const categoryOccurrences = categoryName.reduce(
+    (prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev),
+    {}
+  )
+  const chartData = {
+    datasets: [
+      {
+        label: 'Categories',
+        data: categoryOccurrences,
+        borderWidth: 2,
+        backgroundColor: ['#ffbb1', '#c0c0c0', '#50Af95', '#f3ba2f', '#2a71d0'],
+      },
+    ],
+  }
   return (
     <>
+      <Chart className="mb-8 mt-2" chartData={chartData} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
